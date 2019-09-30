c305c4901cb116147c9f22acb91024274245eab3
yonathanzhong <yonathanzhong@gmail.com>
2019-10-01 00:56:28 +0800

    basic profile page

--- src/app/models/accounts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Accounts } from './accounts';
-
-describe('Accounts', () => {
-  it('should create an instance', () => {
-    expect(new Accounts()).toBeTruthy();
-  });
-});
--- src/app/models/accounts.ts
+++
@@ -1,7 +0,0 @@
-export class Accounts {
-    firstname: string;
-    lastname: string;
-    email: string;
-    password: string;
-    username: string;
-}
