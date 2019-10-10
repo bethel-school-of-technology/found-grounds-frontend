0999fc3b74efab595037e333afa04cec5c78ec3e
Ian King <ianeking3@gmail.com>
2019-10-09 22:19:54 -0700

    router and new front page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
+import { ProfilesModule } from './modules/fullprofilepage/profiles/profiles.module';
+import { HomeComponent } from './shared/components/home/home.component';
+import { SignupComponent } from './shared/components/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+     path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfilesModule
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './shared/components/signup/signup.component';
-import { FullprofilepageModule } from './modules/fullprofilepage/fullprofilepage.module'
+import { FullprofilepageModule } from './modules/fullprofilepage/fullprofilepage.module';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './shared/components/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
