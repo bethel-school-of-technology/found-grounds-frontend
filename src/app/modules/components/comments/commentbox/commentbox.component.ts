6d6f37cc851155c796831f1a1d5d64439052589c
yonathanzhong <yonathanzhong@gmail.com>
2019-10-24 17:11:10 +0700

    comments automatically refresh once a new comment is entered

--- src/app/modules/components/comments/commentbox/commentbox.component.ts
+++ src/app/modules/components/comments/commentbox/commentbox.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Comment } from '../../../../shared/models/comment';
 import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-commentbox',
@@ -11,9 +12,42 @@ export class CommentboxComponent implements OnInit {
   @Input() postId: number;
   @Input() token: number;
   
-  constructor() { }
+  private commentsRoute = 'http://localhost:3000/comments';
+  public comments : Comment [];
 
-  ngOnInit() {
+  constructor(private http: HttpClient) { }
+  // Getting Comments
+  getComments(postId){
+    this.http.get<Comment[]>(this.commentsRoute + "?postId=" + postId).subscribe(comments => {
+      this.comments = comments;
+    })
   }
+
+  // Uploading Comments
+  public commentToPost: Comment;
+  isAdded: boolean = false;
+  confirmationString: string = "New comment has been uploaded";
+  uploadNewComment(comment){
+    this.commentToPost = {
+      "text": comment.text,
+      "postId": this.postId,
+      "userId": this.token,
+      "commentId": comment.id,
+      "id": comment.id,
+      "deleted": false,
+      "timePosted": moment()
+    }
+    this.http.post(this.commentsRoute, this.commentToPost)
+    .subscribe(res => (this.isAdded = true));
+    this.ngOnInit();
+    // .then(()=>{ this.router.navigate([`/${this.id}`])})
+    // add above code once router is done
+    ;
+  }
+  ngOnInit() {
+    this.getComments(this.postId);
+  };
+
+ 
 
 }
