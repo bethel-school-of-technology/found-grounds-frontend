d36303175d4f3733ad8e3e56f025c40d8f4e72d4
yonathanzhong <yonathanzhong@gmail.com>
2019-10-24 17:13:34 +0700

    forgot one last edit for comments to work

--- src/app/modules/components/comments/comments.module.ts
+++ src/app/modules/components/comments/comments.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DisplayCommentsComponent } from './display-comments/display-comments.component';
 import { UsersModule } from '../users/users.module';
-import { UploadCommentsComponent } from './upload-comments/upload-comments.component';
 import { CommentboxComponent } from './commentbox/commentbox.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [DisplayCommentsComponent, UploadCommentsComponent, CommentboxComponent],
+  declarations: [CommentboxComponent],
   imports: [
     CommonModule, UsersModule, FormsModule
   ],
-  exports: [DisplayCommentsComponent, UploadCommentsComponent, CommentboxComponent]
+  exports: [CommentboxComponent]
 })
 export class CommentsModule { }
