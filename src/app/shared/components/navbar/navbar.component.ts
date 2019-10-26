dbe8ad250355df7d8750cca7556f13b9a94dfdb6
yonathanzhong <yonathanzhong@gmail.com>
2019-10-26 09:57:19 +0700

    responsive nav

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -10,6 +10,31 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  showBox(){
+    var x = document.getElementById('navDemo');
+    if (x.className.indexOf("show") == -1) {
+      x.className += " show";
+      x.previousElementSibling.className += " w3-theme-d1";
+    } else {
+      x.className = x.className.replace("show", "");
+      x.previousElementSibling.className =
+      x.previousElementSibling.className.replace(" w3-theme-d1", "");
+    }
+  }
+  
+  showsmallLogin(){
+    document.getElementById('id01').style.display='block';
+    var x = document.getElementById('navDemo');
+    if (x.className.indexOf("show") == -1) {
+      x.className += " show";
+      x.previousElementSibling.className += " w3-theme-d1";
+    } else {
+      x.className = x.className.replace("show", "");
+      x.previousElementSibling.className =
+      x.previousElementSibling.className.replace(" w3-theme-d1", "");
+    }
+  }
+
   ngOnInit() {
   }
   
