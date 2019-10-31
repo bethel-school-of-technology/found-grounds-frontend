493013f5cd35b6ad88299e22360590168ee6981f
yonathanzhong <yonathanzhong@gmail.com>
2019-10-31 23:29:21 +0800

    last changes

--- src/app/modules/signuppages/signup-userpage/signup-userpage.component.ts
+++ src/app/modules/signuppages/signup-userpage/signup-userpage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-userpage',
@@ -7,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupUserpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  createUser(use){
+    if(confirm("Are you ready to find a whole new world of coffee?")){
+      const newUser = {
+        "userId": use.userId,
+        "firstName": use.firstName,
+        "lastName": use.lastName,
+        "imageUrl": use.imageUrl,
+        "city": use.city,
+        "state": use.state,
+        "bio": use.bio,
+        "birthday": use.birthday,
+        "username": use.username,
+        "deleted": false,
+        "roleId": 1
+      }
+      const url = 'http://localhost:8080/api/users';
+      return this.http.post(url, newUser)
+      .toPromise().then(()=>{
+        alert("You're all signed up!")
+        this.router.navigate(['home']);})
+    }
+  }
 
   ngOnInit() {
   }
